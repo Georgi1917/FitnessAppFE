62b2eee57e314c837a5dba485b244923038c9e52
Gosho Zvqr <mamarida15@abv.bg>
2025-01-31 19:04:12 +0200

    feat: finished bar chart for calories; now displays custom bar colors according to daily calories

--- app/calorie-chart.jsx
+++ app/calorie-chart.jsx
@@ -7,7 +7,7 @@ function  CalorieChart() {
         <View style={{justifyContent: "center", alignItems: "center", marginTop: 20}}>
             <View style={styles.innerContainer}>
                 <Text style={styles.textStyle}>Calories</Text>
-                <Text style={[styles.textStyle, styles.goalText]}>Daily Calorie Goal: 3500</Text>
+                <Text style={[styles.textStyle, styles.goalText]}>Daily Calorie Goal: {caloriesGoal}</Text>
             </View>
             <BarChart
                 data={barData}
@@ -15,34 +15,44 @@ function  CalorieChart() {
                 height={220}
                 yAxisLabel={"#"}
                 chartConfig={chartConfig}
-                verticalLabelRotation={30}
                 fromZero
+                withCustomBarColorFromData={true}
+                flatColor={true}
                 style={{
                     borderRadius: 16,
                 }}
+                showValuesOnTopOfBars
+                showBarTops={false}
             />
         </View>
     )
 
 }
 
+const caloriesGoal = 3000;
+const dataValues = [3100, 2000, 1900, 1500, 2500, 3000, 3500]
+
 const barData = {
     labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
     datasets: [
         {
-            data: [2300, 2000, 1900, 1500, 2500, 3000, 3500]
+            data: dataValues,
+            colors: dataValues.map((value) => (
+                value > caloriesGoal ? (opacity = 1) => `rgba(224, 254, 16, ${opacity})` : (opacity = 1) => `rgba(0, 255, 255, ${opacity})`
+            ))
         }
     ]
 }
 
 const chartConfig = {
-    backgroundColor: '#1E2923',
-    backgroundGradientFrom: '#08130D',
-    backgroundGradientTo: '#1F1E30',
+    backgroundColor: '#2A2E37',
+    backgroundGradientFrom: '#2A2E37',
+    backgroundGradientTo: '#2A2E37',
     decimalPlaces: 0,
     color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
     labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-    barPercentage: 0.5,
+    barPercentage: 0.7,
+    barRadius: 5,
 }
 
 const styles = StyleSheet.create(
