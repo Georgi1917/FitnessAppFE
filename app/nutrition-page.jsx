458e017a868a2ab72cd9540e3e90af121962b13b
Gosho Zvqr <mamarida15@abv.bg>
2025-02-02 19:03:11 +0200

    started work on nutrition page

--- app/nutrition-page.jsx
+++ app/nutrition-page.jsx
@@ -1,18 +1,47 @@
-import { View, Text } from "react-native"
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native"
 
 
 function NutritionPage() {
 
     return(
-        <View>
+        <View style={styles.outerContainer}>
 
-            <Text>
-                Nutrition Page
+            <Text style={styles.textStyle}>
+                Meal 1
             </Text>
             
+            <TouchableOpacity style={styles.buttonStyle}>
+                <Text style={styles.buttonText}>
+                    Add Meal
+                </Text>
+            </TouchableOpacity>
+
         </View>
     )
 
 }
+
+const styles = StyleSheet.create(
+    {
+        outerContainer: {
+            flex: 1,
+            backgroundColor: "#1C1C1C",
+        },
+        textStyle: {
+            color: "#fff",
+        },
+        buttonStyle: {
+            backgroundColor: "#E0FE10",
+            padding: 10,
+            margin: 10,
+            borderRadius: 10,
+        },
+        buttonText: {
+            color: "#1D2228",
+            textTransform: "uppercase",
+            textAlign: "center",
+        }
+    }
+)
 
 export default NutritionPage;
