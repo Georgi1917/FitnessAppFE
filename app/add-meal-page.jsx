6d1fe57f98f52fbbc35a9b96cfcc939866973e53
Gosho Zvqr <mamarida15@abv.bg>
2025-02-07 19:21:40 +0200

    feat: add meal page finished

--- app/add-meal-page.jsx
+++ app/add-meal-page.jsx
@@ -1,15 +1,65 @@
-import { View, Text } from "react-native";
+import { View, Text, TextInput, StyleSheet, TouchableOpacity } from "react-native";
 
 function AddMeal() {
 
     return (
-        <View>
-            <Text>
-                Add Meal Page.
-            </Text>
+        <View style={styles.outerContainer}>
+
+            <TextInput style={styles.textInput} placeholder="Search For A Meal"/>
+
+            <View style={styles.buttonContainer}>
+
+                <TouchableOpacity style={styles.buttonStyle}>
+
+                    <Text style={styles.buttonText}>
+                        Search For Meal
+                    </Text>
+
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.buttonStyle}>
+
+                    <Text style={styles.buttonText}>
+                        Add Custom Meal
+                    </Text>
+
+                </TouchableOpacity>
+
+            </View>
+
         </View>
     )
 
 }
+
+const styles = StyleSheet.create(
+    {
+        outerContainer: {
+            flex: 1,
+            backgroundColor: "#1C1C1C",
+        },
+        textInput: {
+            backgroundColor: "#fff",
+            margin: 10,
+            borderRadius: 12,
+        },
+        buttonContainer: {
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+        },
+        buttonStyle: {
+            backgroundColor: "#E0FE10",
+            padding: 10,
+            margin: 10,
+            borderRadius: 10,
+        },
+        buttonText: {
+            color: "#1D2228",
+            textTransform: "uppercase",
+            textAlign: "center",
+        }
+    }
+)
 
 export default AddMeal;
